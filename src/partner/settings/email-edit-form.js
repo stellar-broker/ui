c8d1807bd1c904df58c70938debb3a9522717c3a
yazadzhy <yazadzhy@stellar.expert>
2025-03-11 22:54:53 +0200

    Change partner settings view Add account deletion

--- src/partner/settings/email-edit-form.js
+++ src/partner/settings/email-edit-form.js
@@ -2,37 +2,58 @@ import React, {useCallback, useState} from 'react'
 import {getAuth} from '../../api/auth'
 import {useAutoFocusRef} from '../../utils/hooks/auto-focus-ref'
 import {Button} from '../../components/ui/button'
+import {Dialog} from '../../components/ui/dialog'
 import validateEmail from '../../utils/validate-email'
 
 function EmailEditForm() {
     const userData = getAuth()
     const [email, setEmail] = useState(userData.email)
-    const [isChanged, setIsChanged] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
     const [isValid, setIsValid] = useState(false)
+
+    const toggleDialog = useCallback(() => setIsOpen(prev => !prev), [])
 
     const changeEmail = useCallback(e => {
         const val = e.target.value.trim()
         setEmail(val)
         setIsValid(validateEmail(val))
-        setIsChanged(val !== userData.email)
     }, [])
 
     const updateEmail = useCallback(() => {
         //TODO update email use API
+        toggleDialog()
         notify({type: "success", message: "Email changed successfully"})
     }, [email])
 
-    return <div className="row">
-        <div className="column column-33">
-            <div className="space">
-                <p className="label text-small">Email</p>
-                <input value={email || ''} onChange={changeEmail} ref={useAutoFocusRef} className="styled-input"/>
+    const onKeyDown = useCallback(e => {
+        if (e.keyCode === 13 && isValid) {
+            updateEmail()
+        }
+    }, [isValid])
+
+    return <div>
+        <div className="micro-space">
+            <p className="label text-small">Email</p>
+            <div>
+                <strong>{email}</strong>&emsp;
+                <a href="#" onClick={toggleDialog}>change</a>
             </div>
         </div>
-        <div className="column column-33">
-            <div className="label-space"/>
-            {isChanged && <Button disabled={!isValid} onClick={updateEmail}>Save</Button>}
-        </div>
+        <Dialog dialogOpen={isOpen} className="text-left">
+            <div className="micro-space"><h5>Change email</h5></div>
+            <div className="space">
+                <p className="label text-small">Email</p>
+                <input value={email || ''} onChange={changeEmail} ref={useAutoFocusRef} onKeyDown={onKeyDown} className="styled-input"/>
+            </div>
+            <div className="row">
+                <div className="column column-33 column-offset-33">
+                    <Button outline block onClick={toggleDialog}>Cancel</Button>
+                </div>
+                <div className="column column-33">
+                    <Button block disabled={!isValid} onClick={updateEmail}>Save</Button>
+                </div>
+            </div>
+        </Dialog>
     </div>
 }
 
--- src/partner/settings/settings-form.js
+++ src/partner/settings/settings-form.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import EmailEditForm from './email-edit-form'
 import PasswordEditForm from './password-edit-form'
+import AccountDeleteView from './account-delete-view'
 
 function SettingsForm() {
 
     return <>
         <EmailEditForm/>
-        <div className="hr space"/>
         <PasswordEditForm/>
+        <div className="hr space"/>
+        <AccountDeleteView/>
     </>
 }
 
